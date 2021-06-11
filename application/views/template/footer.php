9f613ea2e4557179e6425d9ca8e794dce39311da
Putra <wahyufs996@gmail.com>
2021-06-11 11:28:02 +0700

    Add Copyright
    copyright template paper dashboard bootstrap

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,9 +1,30 @@
 </div>
 </div>
 </div>
+<footer class="footer footer-black  footer-white " style="position:inherit;bottom:0">
+        <div class="container-fluid">
+          <div class="row">
+            <nav class="footer-nav">
+              <ul>
+                <li><a href="https://www.creative-tim.com" target="_blank">Creative Tim</a></li>
+                <li><a href="https://www.creative-tim.com/blog" target="_blank">Blog</a></li>
+                <li><a href="https://www.creative-tim.com/license" target="_blank">Licenses</a></li>
+              </ul>
+            </nav>
+            <div class="credits ml-auto">
+              <span class="copyright">
+                © <script>
+                  document.write(new Date().getFullYear())
+                </script>, made with <i class="fa fa-heart heart"></i> by Creative Tim
+              </span>
+            </div>
+          </div>
+        </div>
+      </footer>
+</div>
 
 </div>
-</div>
+
 <!--   Core JS Files   -->
 <script src="<?= base_url() ?>assets/js/core/jquery.min.js"></script>
 <script src="<?= base_url() ?>assets/js/core/popper.min.js"></script>
